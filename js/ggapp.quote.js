6c68c0906b9c65cd8a1e93d9f81dd3c527e6c86b
Greg Goodman <greg@goodmandigital.com>
2017-03-19 13:20:39 -0700

    Cleaning up comments

--- js/ggapp.quote.js
+++ js/ggapp.quote.js
@@ -1,4 +1,3 @@
-// This is our Javascript file to call our Quote API
 $(document).ready(function() {
     function ggstrip(html) {
         var tmp = document.createElement("DIV");
@@ -19,14 +18,11 @@ $(document).ready(function() {
                 $('#quoteAuthor').text(quoteAuthor);
                 $('#quoteText').html(quoteText);
 
-                //tweetHtml = '<a class="twitter-share-button" id="twitterLink" href="https://twitter.com/share" data-text="';
                 var tweetText = quoteText;
 
                 if (quoteAuthor.length > 0) {
                     tweetText += ' -' + quoteAuthor;
                 }
-
-                //tweetHtml += '">Share on Twitter</a>';
 
                 var newLink = document.createElement("a");
                 var node = document.createTextNode("Share on Twitter");
@@ -35,8 +31,6 @@ $(document).ready(function() {
                 newLink.id = "twitterLink";
                 newLink.href = "https://twitter.com/share";
                 newLink.dataset.text = ggstrip(tweetText);
-                // newLink.dataset.text = tweetText;
-
 
                 var elems = document.getElementsByClassName('twitter-share-button');
                 elems[0].parentNode.replaceChild(newLink, elems[0]);
@@ -44,24 +38,10 @@ $(document).ready(function() {
                     elems[1].parentNode.removeChild(elems[1]);
                 }
 
-                //                elem = document.getElementById('twitterLink');
-                //                elem.parentNode.removeChild(elem);
-                //$('#twitter-share-button').html(tweetHtml);
                 twttr.widgets.load();
             },
             cache: false
         });
-
-        /*        $.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/", function(json) {
-
-                    if (json) {
-                        quoteText = json.quote;
-                        quoteAuthor = "-" + json.author;
-                    }
-                    $("#quoteText").html(quoteText);
-                    $("#quoteAuthor").html(quoteAuthor);  
-        */
-
     }
 
     getTheQuote();
